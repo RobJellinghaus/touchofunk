b0bab73a44dfabcd93dd5b2aa9cb2cc76d734f4d
DESKTOP-NM8KA7G\Rob Jellinghaus <rjellinghaus@gmail.com>
2016-12-18 22:53:52 -0800

    Rotating cubey paradey thing!

---
+++ Touchofunk Unity/TouchofunkUnity/Assets/InputHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                Collider collider = hit.collider;
+                Rotator rotator = (Rotator)collider.GetComponentInChildren(typeof(Rotator));
+                if (rotator != null)
+                {
+                    rotator.Rotating = !((Rotator)rotator).Rotating;
+
+                    if (rotator.Rotating && !rotator.Cloned)
+                    {
+                        Instantiate(collider, collider.transform.position + new Vector3(2f, 0f, 0f), collider.transform.localRotation);
+                        rotator.Cloned = true;
+                    }
+                }
+            }
+        }
+	}
+}
--- Touchofunk Unity/TouchofunkUnity/Assets/Rotator.cs
+++ Touchofunk Unity/TouchofunkUnity/Assets/Rotator.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour {
+    public bool Rotating;
+    public bool Cloned;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        Rotating = true;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        transform.Rotate(new Vector3(0f, 1f, 1f), 5f, Space.Self);
+	void Update ()
+    {
+        if (Rotating)
+        {
+            transform.Rotate(new Vector3(0f, 1f, 1f), 5f, Space.Self);
+        }
 	}
 }
